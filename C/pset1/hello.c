8d9cae241bb209630319402ee669cd92ffb9167c
lawrencedrums <51978449+lawrencedrums@users.noreply.github.com>
2020-06-01 17:47:43 +0800

    Create hello.c

---
+++ C/pset1/hello.c
@@ -0,0 +1,11 @@
+//13 May, 2020.
+//Lawrence Wong. CS50x. Hello.
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What is your name?\n");
+    printf("Hello, %s!\n", name);
+}
